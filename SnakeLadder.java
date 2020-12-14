94a65bdf0b639349663305a76b8c966f9e2aa4f9
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 22:55:41 +0530

    Added : Option check

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,22 @@ class SnakeLadder
 		int position=0;
 		int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
 		System.out.println("Die Rolled:"+dieRoll);
+		int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
+		if(optionSelect==1)
+		{
+			System.out.println("**No Play**");
+		}
+		else if(optionSelect==2)
+		{
+			System.out.println("$$$$$Hurrah Ladder Comes$$$$");
+			position+=dieRoll;
+
+		}
+		else
+		{
+			System.out.println("####Opps Snake Comes####");
+			position-=dieRoll;
+		}
 	}
 
 }
