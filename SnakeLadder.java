ba49b68fb09c02e1bc4cbaf08202cf27b53c5af3
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 22:33:59 +0530

    Added : Roll die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,8 @@ class SnakeLadder
 	public static void main(String args[])
 	{
 		int position=0;
+		int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+		System.out.println("Die Rolled:"+dieRoll);
 	}
 
 }
