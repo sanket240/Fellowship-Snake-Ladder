0bb81684dd90aa2a01a63c5a5e0159a958fbb0ec
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 23:49:00 +0530

    Added : Position of dice and no of times dice rolled

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,10 +2,11 @@ class SnakeLadder
 {
 	public static void main(String args[])
 	{
-		int position=0;
+		int position=0,Count=0;
 		while(position!=100)
 		{
 			int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+			Count++;
 			System.out.println("Die Rolled:"+dieRoll);
 			int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
 			if(optionSelect==1)
@@ -37,6 +38,8 @@ class SnakeLadder
 					position-=dieRoll;
 				}
 			}
+			System.out.println("Die roll count:"+Count);
+			System.out.println("Position is:"+position);
 			
 		}
 	}
