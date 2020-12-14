38bccfc282564c5c7bcb59358706dd7f4d01ef75
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 23:26:03 +0530

    Added : Repeat till 100 wins

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,23 +3,37 @@ class SnakeLadder
 	public static void main(String args[])
 	{
 		int position=0;
-		int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
-		System.out.println("Die Rolled:"+dieRoll);
-		int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
-		if(optionSelect==1)
+		while(position!=100)
 		{
-			System.out.println("**No Play**");
-		}
-		else if(optionSelect==2)
-		{
-			System.out.println("$$$$$Hurrah Ladder Comes$$$$");
-			position+=dieRoll;
+			int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+			System.out.println("Die Rolled:"+dieRoll);
+			int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
+			if(optionSelect==1)
+			{
+				System.out.println("**No Play**");
+			}
+			else if(optionSelect==2)
+			{
+				System.out.println("$$$$$Hurrah Ladder Comes$$$$");
+				if(position+dieRoll<=100)
+				{
+					position+=dieRoll;
+				}
 
-		}
-		else
-		{
-			System.out.println("####Opps Snake Comes####");
-			position-=dieRoll;
+			}
+			else
+			{
+				System.out.println("####Opps Snake Comes####");
+				if((position-dieRoll)<0)
+				{
+					position=0;
+				}
+				else
+				{
+					position-=dieRoll;
+				}
+			}
+			
 		}
 	}
 
